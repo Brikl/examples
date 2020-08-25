4394e319e94f7310368d1539fc658d7cfa599a96
Phoomparin Mano <phoomparin@gmail.com>
2020-08-25 18:02:19 +0700

    fix: remove unnecessary productDesignId

--- apps/gatsby/src/pages/index.tsx
+++ apps/gatsby/src/pages/index.tsx
@@ -13,7 +13,6 @@ export const Index = () => {
             style={{ height: '100vh' }}
             shopId="demo"
             productId="56bbcb76-c6ee-44ae-92d7-659fde38fa9b"
-            productDesignId="b5bbe2df-750b-4c15-a426-48c215fd3efa"
           />
         </div>
       </main>
