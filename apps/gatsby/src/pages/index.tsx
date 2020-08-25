4a7f5122496aba7362788c29388ed235148c916c
Phoomparin Mano <phoomparin@gmail.com>
2020-08-25 17:58:34 +0700

    feat: display the demo as full height

--- apps/gatsby/src/pages/index.tsx
+++ apps/gatsby/src/pages/index.tsx
@@ -10,7 +10,7 @@ export const Index = () => {
       <main className="main">
         <div className="container">
           <DesignStudio
-            style={{ height: 600 }}
+            style={{ height: '100vh' }}
             shopId="demo"
             productId="56bbcb76-c6ee-44ae-92d7-659fde38fa9b"
             productDesignId="b5bbe2df-750b-4c15-a426-48c215fd3efa"
